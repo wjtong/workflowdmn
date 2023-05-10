8770d6f7911695533c46501063b4ce67b14a7ed6
Wenji Tong <wtong@gmail.com>
2023-05-10 18:08:10 +0800

    initialized project

---
+++ src/main/java/com/banfftech/dmn/WorkFlow.java
@@ -0,0 +1,9 @@
+package com.banfftech.dmn;
+
+public class WorkFlow {
+    private String statusId;
+    private String activeTaskName;
+    private String activeTaskStatusId;
+    private String nextTaskName;
+
+}
