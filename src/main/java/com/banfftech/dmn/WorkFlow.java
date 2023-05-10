106d9672d670d38ae47c0794b1d9449b438afa53
Wenji Tong <wtong@gmail.com>
2023-05-10 18:37:38 +0800

    keep working

--- src/main/java/com/banfftech/dmn/WorkFlow.java
+++
@@ -1,9 +0,0 @@
-package com.banfftech.dmn;
-
-public class WorkFlow {
-    private String statusId;
-    private String activeTaskName;
-    private String activeTaskStatusId;
-    private String nextTaskName;
-
-}
